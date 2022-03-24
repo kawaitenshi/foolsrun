d0fe1da2777fce35d407441e23315230fbb08f73
Evan Kanter <evankanter28@gmail.com>
2022-03-24 01:31:05 -0400

    fix: fix main menu scene activation issue

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,11 +10,13 @@ public class MainMenu : MonoBehaviour
     private string _firstLevelName = "MainScene";
     public GameObject startButton;
     public GameObject LoadingText;
-  
+    private bool _sceneChanged = false;
+
+    private Scene _firstLevel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _firstLevel = SceneManager.GetSceneByName(_firstLevelName);
     }
 
     // Update is called once per frame
@@ -25,6 +27,13 @@ public class MainMenu : MonoBehaviour
         {
             NewGame();
         }
+
+        if (_sceneChanged && _firstLevel.isLoaded)
+        {
+            SceneManager.SetActiveScene(_firstLevel);
+            AsyncOperation unload = SceneManager.UnloadSceneAsync("MainMenu");
+            _sceneChanged = false;
+        }
         
     }
 
@@ -34,6 +43,6 @@ public class MainMenu : MonoBehaviour
         LoadingText.SetActive(true);
         startButton.SetActive(false);
         SceneManager.LoadScene(_firstLevelName);
-        AsyncOperation unload = SceneManager.UnloadSceneAsync("MainMenu");
+        _sceneChanged = true;
     }
 }
