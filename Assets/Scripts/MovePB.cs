377f10ce50c134527eff2a7008c6d555094d887e
KeweiQ <kewei.qiu@outlook.com>
2022-03-24 01:17:46 -0400

    Revert "Fix objects in hallway"

--- Assets/Scripts/MovePB.cs
+++ Assets/Scripts/MovePB.cs
@@ -84,67 +84,67 @@ public class MovePB : MonoBehaviour
         Animator.SetBool("isIdle", !movingForward && isGrounded && !hasFallen);
         Animator.SetBool("fallen", hasFallen);
 
+        // move 90 degrees right (press only "D" or "D" + "W" + "S")
         if (!hasFallen)
         {
-            // move 90 degrees right (press only "D" or "D" + "W" + "S")
-            if ((Input.GetKey("d") && !Input.GetKey("w") && !Input.GetKey("s")) || (Input.GetKey("d") && Input.GetKey("w") && Input.GetKey("s"))) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, 90, 0);
-                heading = HumanTransform.forward;
-                inputScale = Mathf.Abs(adInput);
+          if ((Input.GetKey("d") && !Input.GetKey("w") && !Input.GetKey("s")) || (Input.GetKey("d") && Input.GetKey("w") && Input.GetKey("s"))) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, 90, 0);
+              heading = HumanTransform.forward;
+              inputScale = Mathf.Abs(adInput);
 
-            // move 90 degrees left (press only "A" or "A" + "W" + "S")
-            } else if ((Input.GetKey("a") && !Input.GetKey("w") && !Input.GetKey("s")) || (Input.GetKey("a") && Input.GetKey("w") && Input.GetKey("s"))) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, -90, 0);
-                heading = HumanTransform.forward;
-                inputScale = Mathf.Abs(adInput);
+          // move 90 degrees left (press only "A" or "A" + "W" + "S")
+          } else if ((Input.GetKey("a") && !Input.GetKey("w") && !Input.GetKey("s")) || (Input.GetKey("a") && Input.GetKey("w") && Input.GetKey("s"))) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, -90, 0);
+              heading = HumanTransform.forward;
+              inputScale = Mathf.Abs(adInput);
 
-            // move 0 degree forward (press only "W" or "W" + "A" + "D")
-            } else if ((Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("d")) || (Input.GetKey("w") && Input.GetKey("a") && Input.GetKey("d"))) {
-                userRotation[1] = cameraRotation[1];
-                heading = HumanTransform.forward;
-                inputScale = Mathf.Abs(wsInput);
+          // move 0 degree forward (press only "W" or "W" + "A" + "D")
+          } else if ((Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("d")) || (Input.GetKey("w") && Input.GetKey("a") && Input.GetKey("d"))) {
+              userRotation[1] = cameraRotation[1];
+              heading = HumanTransform.forward;
+              inputScale = Mathf.Abs(wsInput);
 
-            // move 180 degrees backward (press only "S" or "S" + "A" + "D")
-            } else if ((Input.GetKey("s") && !Input.GetKey("a") && !Input.GetKey("d")) || (Input.GetKey("s") && Input.GetKey("a") && Input.GetKey("d"))) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, 180, 0);
-                heading = HumanTransform.forward;
-                inputScale = Mathf.Abs(wsInput);
+          // move 180 degrees backward (press only "S" or "S" + "A" + "D")
+          } else if ((Input.GetKey("s") && !Input.GetKey("a") && !Input.GetKey("d")) || (Input.GetKey("s") && Input.GetKey("a") && Input.GetKey("d"))) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, 180, 0);
+              heading = HumanTransform.forward;
+              inputScale = Mathf.Abs(wsInput);
 
-            // move 45 degrees right (press "W" + "D")
-            } else if (Input.GetKey("w") && Input.GetKey("d")) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, 45, 0);
-                heading = HumanTransform.forward;
-                inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
+          // move 45 degrees right (press "W" + "D")
+          } else if (Input.GetKey("w") && Input.GetKey("d")) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, 45, 0);
+              heading = HumanTransform.forward;
+              inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
 
-            // move 45 degrees left (press "W" + "A")
-            } else if (Input.GetKey("w") && Input.GetKey("a")) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, -45, 0);
-                heading = HumanTransform.forward;
-                inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
+          // move 45 degrees left (press "W" + "A")
+          } else if (Input.GetKey("w") && Input.GetKey("a")) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, -45, 0);
+              heading = HumanTransform.forward;
+              inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
 
-            // move 135 degrees right (press "S" + "D")
-            } else if (Input.GetKey("s") && Input.GetKey("d")) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, 135, 0);
-                heading = HumanTransform.forward;
-                inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
+          // move 135 degrees right (press "S" + "D")
+          } else if (Input.GetKey("s") && Input.GetKey("d")) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, 135, 0);
+              heading = HumanTransform.forward;
+              inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
 
-            // move 135 degrees left (press "S" + "A")
-            } else if (Input.GetKey("s") && Input.GetKey("a")) {
-                userRotation[1] = cameraRotation[1];
-                userRotation += new Vector3(0, -135, 0);
-                heading = HumanTransform.forward;
-                inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
+          // move 135 degrees left (press "S" + "A")
+          } else if (Input.GetKey("s") && Input.GetKey("a")) {
+              userRotation[1] = cameraRotation[1];
+              userRotation += new Vector3(0, -135, 0);
+              heading = HumanTransform.forward;
+              inputScale = (Mathf.Abs(wsInput) + Mathf.Abs(adInput)) / 2.0f;
 
-            // stand still
-            } else {
-                inputScale = 0;
-            }
+          // stand still
+          } else {
+              inputScale = 0;
+          }
         }
 
         // rotate character to the right direction
@@ -190,7 +190,6 @@ public class MovePB : MonoBehaviour
         // bool hitGround = Physics.BoxCast(HumanCollider.bounds.center, HumanTransform.lossyScale, HumanTransform.up * -1, Quaternion.Euler(Vector3.zero), HumanTransform.lossyScale.y + extraHeight);
         bool hitGround = Physics.Raycast(HumanTransform.position, Vector3.down, 0.2f);
 
-        /*
         Color rayColor;
         if (hitGround) {
             rayColor = Color.green;
@@ -198,7 +197,6 @@ public class MovePB : MonoBehaviour
             rayColor = Color.red;
         }
         Debug.DrawRay(HumanTransform.position, Vector3.down * 0.2f, rayColor);
-        */
 
         return hitGround;
     }
