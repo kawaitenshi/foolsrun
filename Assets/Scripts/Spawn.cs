297acfec42ed5422514ac21faea978ee0d84dc17
KeweiQ <kewei.qiu@outlook.com>
2022-03-23 23:26:50 -0400

    add first version of hallway judgement, need debug

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -4,49 +4,51 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-  public GameObject [] spawnObjects; // The object to spawn repeatedly
-  public float timeToSpawn; // Time in seconds before the next spawn of spawnObject
-  private Vector3 position; // position to spawn
-  private GameObject player; // The active character right now
-  private bool spawnStarted; // Have we already started spawning?
-  private int activePotion; // The potion in the spawnObjects index that is currently spawning
+    public GameObject [] spawnObjects; // The object to spawn repeatedly
+    public float timeToSpawn; // Time in seconds before the next spawn of spawnObject
+    private Vector3 position; // position to spawn
+    private GameObject player; // The active character right now
+    private bool spawnStarted; // Have we already started spawning?
+    private int activePotion; // The potion in the spawnObjects index that is currently spawning
 
-  IEnumerator spawn () {
-      while(true) {
-        spawnStarted = true;
-        // Set the position of the spawn object as the position of the player
-        position = player.transform.position;
-        position += player.transform.forward * 3;
-        position += player.transform.up * 7;
+    IEnumerator spawn () {
+        while (!PlayerCollision.inHallway) {
+            spawnStarted = true;
+            // Set the position of the spawn object as the position of the player
+            position = player.transform.position;
+            position += player.transform.forward * 3;
+            position += player.transform.up * 7;
 
 
-        Debug.Log("Instantiating object " + spawnObjects[activePotion].name + " in location "
-                  + position.x.ToString() + " "
-                  + position.y.ToString() + " "
-                  + position.z.ToString());
+            Debug.Log("Instantiating object " + spawnObjects[activePotion].name + " in location "
+                        + position.x.ToString() + " "
+                        + position.y.ToString() + " "
+                        + position.z.ToString());
 
-        GameObject potion  = Instantiate(spawnObjects[activePotion],
-                                         position,
-                                         Quaternion.identity);
+            GameObject potion  = Instantiate(spawnObjects[activePotion],
+                                                position,
+                                                Quaternion.identity);
 
-        Debug.Log("Waiting for " + timeToSpawn.ToString() + " seconds.");
-        /** Wait for some time before spawning again.
-        In case of the potion, the time to spawn again
-        should decrease as the player levels up */
-         yield return new WaitForSeconds (timeToSpawn);
-       }
-  }
+            Debug.Log("Waiting for " + timeToSpawn.ToString() + " seconds.");
+            /** Wait for some time before spawning again.
+            In case of the potion, the time to spawn again
+            should decrease as the player levels up */
+            yield return new WaitForSeconds (timeToSpawn);
+        }
+    }
 
-  public void setActivePlayer(GameObject activePlayer)
-  {
-    if (spawnStarted) print("Player is :" +player.ToString());
-    player = activePlayer;
-    print("Setting the player to active player: " + activePlayer.ToString());
-    if (!spawnStarted) StartCoroutine(spawn());
-  }
+    public void setActivePlayer (GameObject activePlayer) {
+        if (spawnStarted) {
+            print("Player is :" +player.ToString());
+        }
+        player = activePlayer;
+        print("Setting the player to active player: " + activePlayer.ToString());
+        if (!spawnStarted) {
+            StartCoroutine(spawn());
+        }
+    }
 
-  public void setActivePotion(int potion)
-  {
-    activePotion = potion;
-  }
+    public void setActivePotion (int potion) {
+        activePotion = potion;
+    }
 }
